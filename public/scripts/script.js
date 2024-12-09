c2b6bc24c20a711af99bd06c82b57cd30e4ba4c7
vayntail <113714157+vayntail@users.noreply.github.com>
2024-12-09 09:18:03 -0500

    update on frontend

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -36,7 +36,15 @@ function displayTabs(tabs) {
       getColumnsFromTab(tab);
     }
 
-    // on tab click, set it to active
+    // on tab click, set it to active and refresh board
+    tabEl.addEventListener("click", () => {
+      // for each tab in aside, remove active
+      aside.childNodes.forEach((child) => {
+        child.classList.remove("tab-active");
+      });
+      tabEl.classList.add("tab-active");
+      getColumnsFromTab(tab);
+    });
   });
 
   // create add new tab button
@@ -94,10 +102,10 @@ async function getColumnsFromTab(tab) {
 
 function displayColumns(tabId, columns) {
   console.log(columns);
-  const main = document.querySelector("main");
+  const columnsEl = document.querySelector("#columns");
 
-  // clear all elements inside main
-  main.innerHTML = "";
+  // clear all elements inside columnsEl
+  columnsEl.innerHTML = "";
 
   // for each column, create and add column
   columns.forEach((column, index) => {
@@ -105,25 +113,41 @@ function displayColumns(tabId, columns) {
     const columnEl = document.createElement("div");
     columnEl.classList.add("column");
     columnEl.innerText = column.name;
-    main.append(columnEl);
+    columnsEl.append(columnEl);
 
     // for each column, add cards
     const url = `http://localhost:5052/api/tabs/${tabId}/columns/${column._id}/cards`;
     console.log(url);
-    getCardsFromColumn(url);
+    getCardsFromColumn(columnEl, url);
   });
 
   // create add new column button
+  const addNew = document.createElement("div");
+  addNew.classList.add("column");
+  addNew.style.height = "fit-content";
+  addNew.innerHTML = `
+  <input></input>
+  <button>create</button>
+  `;
+  columnsEl.append(addNew);
 }
 
-async function getCardsFromColumn(url) {
+async function getCardsFromColumn(columnEl, url) {
   try {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
     const cards = await response.json();
-    console.log(cards);
+    console.log("hey" + cards);
+    // display the cards
+    const cardEl = document.createElement("div");
+    cardEl.innerHTML = `
+    <h3>${cards.title}</h3>
+    <p>${cards.content}</p>
+      `;
+    cardEl.classList.add("card");
+    columnEl.append(cardEl);
   } catch (error) {
     console.error(error.message);
   }
